68fa518af677cdcf695ae1a10f77e6da6c367b92
Reza Okhravi <rezaokhravi.ir@gmail.com>
2022-06-25 22:54:05 +0430

    آپدیت دیتابیس

--- Persistence/CleanArch.Data/Migrations/20220625182220_models.Designer.cs
+++ Persistence/CleanArch.Data/Migrations/20220625182220_models.Designer.cs
@@ -4,6 +4,7 @@ using CleanArch.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArch.Data.Migrations
 {
     [DbContext(typeof(EvaluationDBContext))]
-    partial class EvaluationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20220625182220_models")]
+    partial class models
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Persistence/CleanArch.Data/Migrations/20220625182220_models.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArch.Data.Migrations
+{
+    public partial class models : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActiveCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RegisterDate",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "DisplayName");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "UserAvatar",
+                table: "Users",
+                type: "tinyint",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLoginDate",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "DisplayName",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserAvatar",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ActiveCode",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegisterDate",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
